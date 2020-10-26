24a481c5aa3a37fdad7186192fb26009a59c0b7c
João Jeefer <joao.felix@dbserver.com.br>
2020-10-26 14:32:53 -0300

    Rename app

--- android/app/src/main/java/com/realm/MainActivity.java
+++ android/app/src/main/java/com/realm/MainActivity.java
@@ -1,4 +1,4 @@
-package com.realm;
+package com.test;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "realm";
+    return "test";
   }
 }
